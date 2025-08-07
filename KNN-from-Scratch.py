3d0ab4d604d8c4b4a006f37f8aeacf647a07be75
TanjinaHasan15 <tanzinahasantanzinahasan@gmail.com>
2025-08-07 21:16:54 +0600

    Metrics

--- KNN-from-Scratch.py
+++ KNN-from-Scratch.py
@@ -29,4 +29,30 @@ class KNN:
         k_labels = self.y_train[k_indices]
         return Counter(k_labels).most_common(1)[0][0]
 
+# --- Custom Evaluation Metrics ---
+def accuracy_score_custom(y_true, y_pred):
+    return np.sum(np.array(y_true) == np.array(y_pred)) / len(y_true)
 
+def confusion_matrix_custom(y_true, y_pred, labels=None):
+    if labels is None:
+        labels = sorted(set(y_true))
+    matrix = np.zeros((len(labels), len(labels)), dtype=int)
+    label_to_index = {label: idx for idx, label in enumerate(labels)}
+    for t, p in zip(y_true, y_pred):
+        matrix[label_to_index[t]][label_to_index[p]] += 1
+    return matrix
+
+def precision_recall_f1_custom(y_true, y_pred, labels=None):
+    cm = confusion_matrix_custom(y_true, y_pred, labels)
+    precision, recall, f1 = [], [], []
+    for i in range(len(cm)):
+        tp = cm[i][i]
+        fp = cm[:, i].sum() - tp
+        fn = cm[i, :].sum() - tp
+        prec = tp / (tp + fp) if tp + fp else 0
+        rec = tp / (tp + fn) if tp + fn else 0
+        f1_score = 2 * prec * rec / (prec + rec) if prec + rec else 0
+        precision.append(prec)
+        recall.append(rec)
+        f1.append(f1_score)
+    return precision, recall, f1
